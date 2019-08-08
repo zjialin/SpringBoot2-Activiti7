27105c569bb242591a7a5e0996e53452c0fbbd39
zjialin <zjialin@outlook.com>
2019-08-08 11:29:51 +0800

    工作流

--- src/main/java/com/zjialin/workflow/config/SwaggerConfig.java
+++ src/main/java/com/zjialin/workflow/config/SwaggerConfig.java
@@ -17,23 +17,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
     @Value(value = "${swagger.enabled}")
-    Boolean swaggerEnabled;
+    private Boolean swaggerEnabled;
 
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
                 // 是否开启
                 .enable(swaggerEnabled).select()
-                .apis(RequestHandlerSelectors.basePackage("com.reanod.workflow.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.zjialin.workflow.controller"))
                 .paths(PathSelectors.any()).build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Reanod工作流API")
+                .title("Activiti7-工作流API")
                 .description("xugj | 高效码农")
                 // 作者信息
-                .contact(new Contact("高效码农", "https://www.xugj520.cn", "514583562@qq.com"))
+                .contact(new Contact("zjialin", "https://github.com/zjialin/SpringBoot2-Activiti7", "zjialin@outlook.com"))
                 .version("1.0.0")
                 .build();
     }
